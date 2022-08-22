2d121ea886c9554154f587100a8a4a07f62c3926
Liyihan <1456133139@qq.com>
2022-08-22 20:56:36 +0800

    博客管理页面添加分页功能

--- src/main/java/com/controller/admin/BlogController.java
+++ src/main/java/com/controller/admin/BlogController.java
@@ -2,20 +2,21 @@ package com.controller.admin;
 
 import com.bean.Detail;
 import com.bean.User;
+import com.bean.UserDetail;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.service.BlogService;
 import com.service.TagService;
 import com.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * @author Zm-Mmm
@@ -39,13 +40,18 @@ public class BlogController {
      * @return
      */
     @GetMapping("/blogs")
-    public String list(Model model,HttpServletRequest request){
+    public String list(Model model, HttpServletRequest request, @RequestParam(required = false,defaultValue = "1",value = "pageNum")int pageNum){
         // 所有分类
         model.addAttribute("types",typeService.listType());
         // 所有博客
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
-        model.addAttribute("page",blogService.selectDetailFromUserIdLimit(user.getId()));
+        // 分页查询
+        PageHelper.startPage(pageNum,10);
+        List<UserDetail> userDetails = blogService.selectDetailFromUserIdLimit(user.getId());
+        // 得到分页结果对象
+        PageInfo<UserDetail> pageInfo = new PageInfo<>(userDetails);
+        model.addAttribute("page",pageInfo);
         return "admin/admin-blog";
     }
 
