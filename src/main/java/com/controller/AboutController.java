7dff2407c40b4bc60649c4780c7ea1745053c184
Liyihan <1456133139@qq.com>
2022-08-27 23:45:03 +0800

    使用Redis实现每日访客数量、总访客数量统计与持久化定时任务

--- src/main/java/com/controller/AboutController.java
+++ src/main/java/com/controller/AboutController.java
@@ -2,15 +2,21 @@ package com.controller;
 
 import com.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Objects;
 
 /**
  * @author Zm-Mmm
  */
 @Controller
 public class AboutController {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Autowired
     private UserInfoService userInfoService;
@@ -23,7 +29,11 @@ public class AboutController {
     @GetMapping("/about")
     public String about(Model model){
         int count = userInfoService.findUserCount();
+        int visitorCount = stringRedisTemplate.opsForHash().keys("visitorIP").size();
+        int countViews =  Integer.parseInt(Objects.requireNonNull(stringRedisTemplate.opsForValue().get("countViews")));
         model.addAttribute("count",count);
+        model.addAttribute("visitorCount",visitorCount);
+        model.addAttribute("countViews",countViews);
         return "about";
     }
 }
