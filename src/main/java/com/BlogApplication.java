c42fb5e2033b096d0bca27b60c72acc103a397eb
Liyihan <1456133139@qq.com>
2022-08-28 16:29:22 +0800

    删除冗余

--- src/main/java/com/BlogApplication.java
+++ src/main/java/com/BlogApplication.java
@@ -2,11 +2,6 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author Zm-Mmm
