2317eaf9ead3a04792ffade3f076d981af98c059
Zm_Mmm <1456133139@qq.com>
2022-09-24 23:48:09 +0800

    修复留言页页脚位置显示错误

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -14,9 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Zm-Mmm
@@ -24,7 +22,7 @@ import java.util.Map;
 @Controller
 public class IndexController {
 
-    private final BlogService blogService;
+    private final BlogService blogService; 
 
     private final TypeService typeService;
 
