b703c49c185532dff555e225bdc8c92d62e1c6b6
liyihan <zm-mmm@foxmail.com>
2022-10-11 16:39:16 +0800

    修改表主键属性生成策略@GeneratedValue(strategy = GenerationType.IDENTITY)。完善数据库所有表主键自增。 删除JPA自动生成主键绑定表

--- src/main/java/com/bean/Type.java
+++ src/main/java/com/bean/Type.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Type {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
--- src/main/java/com/bean/User.java
+++ src/main/java/com/bean/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class User implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     /**
      * 昵称
