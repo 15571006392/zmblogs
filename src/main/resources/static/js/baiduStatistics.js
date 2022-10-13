985492a912c2d15ac8898f496d8971efd363e506
liyihan <zm-mmm@foxmail.com>
2022-10-13 10:27:17 +0800

    初始化v2.6.10版本，接入百度统计站长工具

---
+++ src/main/resources/static/js/baiduStatistics.js
@@ -0,0 +1,7 @@
+    var _hmt = _hmt || [];
+    (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?3e48938309a6cf2572ab5368150cdcf8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
